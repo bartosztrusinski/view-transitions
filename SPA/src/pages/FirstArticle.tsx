583d3541cc4b310d2ba46954e124d91f49bb814e
Bartosz Trusiński <bartosztrusinski@gmail.com>
2024-11-15 22:34:04 +0100

    create same pages

---
+++ SPA/src/pages/FirstArticle.tsx
@@ -0,0 +1,28 @@
+export function FirstArticle() {
+  return (
+    <main>
+      <h1>First Article</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam quod,
+        voluptates, quae, quos quia quibusdam quidem voluptatum laboriosam
+        doloremque quas doloribus. Quisquam quod, voluptates, quae, quos quia
+        quibusdam quidem voluptatum laboriosam doloremque quas doloribus.
+      </p>
+      <p>
+        Temporibus quos quas voluptate at asperiores, officia explicabo laborum
+        voluptatum omnis. Nemo, impedit asperiores sequi, voluptatem itaque,
+        labore deleniti perferendis dignissimos molestias illo harum. Quia
+        facilis hic dolorum a earum. Vero nemo blanditiis maxime esse, a
+        reprehenderit illo dicta nostrum cum accusamus qui dolor.
+      </p>
+      <p>
+        Quasi at similique adipisci provident, corrupti, neque expedita possimus
+        odio esse reiciendis officia quia voluptatem consectetur? Repellat iste
+        nam aliquid perspiciatis vitae debitis sint maiores unde ullam hic.
+        Culpa repellendus obcaecati, quia omnis reiciendis architecto dolore vel
+        harum assumenda, iusto eum nihil vero ea numquam esse.
+      </p>
+      <a href='/'>Back to Home</a>
+    </main>
+  );
+}
---
+++ SPA/src/pages/MainPage.tsx
@@ -0,0 +1,29 @@
+export function MainPage() {
+  return (
+    <main>
+      <h1>View Transitions</h1>
+      <section>
+        <h2>First Article</h2>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
+          quod, voluptates, quae, quos quia quibusdam quidem voluptatum
+          laboriosam doloremque quas doloribus. Quisquam quod, voluptates, quae,
+          quos quia quibusdam quidem voluptatum laboriosam doloremque quas
+          doloribus.
+        </p>
+        <a href='/first-article'>Read more</a>
+      </section>
+      <hr />
+      <section>
+        <h2>Second Article</h2>
+        <p>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iusto minima
+          voluptatibus iure eveniet incidunt dicta odit iste vero laborum!
+          Perferendis et laboriosam asperiores illum velit quasi nostrum
+          pariatur, accusamus culpa.
+        </p>
+        <a href='/second-article'>Read more</a>
+      </section>
+    </main>
+  );
+}
