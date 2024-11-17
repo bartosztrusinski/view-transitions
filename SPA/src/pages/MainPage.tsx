2ebd00b15f542f0d83e9a4ae5fc86def38dd83c7
Bartosz Trusiński <bartosztrusinski@gmail.com>
2024-11-18 00:57:48 +0100

    add prettier

--- SPA/src/pages/MainPage.tsx
+++ SPA/src/pages/MainPage.tsx
@@ -1,30 +1,28 @@
 import { Link } from '../routing/Link';
 
-export function MainPage() {
+export default function MainPage() {
   return (
     <main>
       <h1>View Transitions</h1>
       <section>
         <h2>First Article</h2>
         <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
-          quod, voluptates, quae, quos quia quibusdam quidem voluptatum
-          laboriosam doloremque quas doloribus. Quisquam quod, voluptates, quae,
-          quos quia quibusdam quidem voluptatum laboriosam doloremque quas
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam quod, voluptates, quae,
+          quos quia quibusdam quidem voluptatum laboriosam doloremque quas doloribus. Quisquam quod,
+          voluptates, quae, quos quia quibusdam quidem voluptatum laboriosam doloremque quas
           doloribus.
         </p>
-        <Link to='/first-article'>Read more</Link>
+        <Link to="/first-article">Read more</Link>
       </section>
       <hr />
       <section>
         <h2>Second Article</h2>
         <p>
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iusto minima
-          voluptatibus iure eveniet incidunt dicta odit iste vero laborum!
-          Perferendis et laboriosam asperiores illum velit quasi nostrum
-          pariatur, accusamus culpa.
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iusto minima voluptatibus iure
+          eveniet incidunt dicta odit iste vero laborum! Perferendis et laboriosam asperiores illum
+          velit quasi nostrum pariatur, accusamus culpa.
         </p>
-        <Link to='/second-article'>Read more</Link>
+        <Link to="/second-article">Read more</Link>
       </section>
     </main>
   );
